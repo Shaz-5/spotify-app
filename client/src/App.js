70658a189c7dcedb2bc5fb03b099da5bfa423c9d
Shaz <k.shaznazar@gmail.comm>
2023-02-04 17:26:24 +0530

    Redirect added

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,12 @@ function App() {
   const refresh_token = new URLSearchParams(window.location.hash).get('refresh_token');
   const expires_in = new URLSearchParams(window.location.hash).get('expires_in');
 
+  useEffect(() => {
+    if (!access_token) {
+      window.location = "http://localhost:8888/login";
+    }
+  }, [access_token]);
+
   return (
     <div>
       <Dashboard  access_token={access_token} refresh_token={refresh_token} expires_in={expires_in}/>
