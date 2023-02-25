e4e60aee58af0bda232a8856068dbcaa39b96b2e
Shaz <k.shaznazar@gmail.comm>
2023-02-26 01:59:50 +0530

    Added some css

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -60,16 +60,16 @@ const Dashboard = (props) => {
             setSearchResults(
                 res.body.tracks.items.map((track)=>{
 
-                    const smallestAlbumImage = track.album.images.reduce((smallest, image)=>{
-                        if(image.height < smallest.height) return image
-                        return smallest
+                    const biggestAlbumImage = track.album.images.reduce((biggest, image)=>{
+                        if(image.height > biggest.height) return image
+                        return biggest
                     }, track.album.images[0]);
 
                     return {
                         artist: track.artists[0].name,
                         title: track.name,
                         uri: track.uri,
-                        albumUrl: smallestAlbumImage.url
+                        albumUrl: biggestAlbumImage.url
                     }
                 })
             )
@@ -84,13 +84,49 @@ const Dashboard = (props) => {
 
     return ( 
         <Fragment>
-            
-            <input type="search" placeholder="Search Songs/Artists" value={search}
-            onChange={(e)=>{setSearch(e.target.value)}}/>
 
-            <div>{ searchResults.map(track=>{
+            <div style={{margin: '0', height: '300px', position:'relative', background: '#191414', maxWidth: '100%'}}>
+                <div style={{ display: "flex", justifyContent: "center" }}>
+                    <h1 style={{margin: '0', 
+                        position: 'absolute', 
+                        color: 'grey', 
+                        padding: '20px', 
+                        left: '50%', 
+                        top: '40%',
+                        fontSize: '40px', 
+                        transform: 'translate(-50%, -50%)'
+                    }}>Play Your <span style={{color: '#1DB954', fontSize: '1.1em'}}>Favourite</span> Music</h1>
+                </div>
+
+                <div style={{ display: "flex", justifyContent: "center" }}>
+                    <input type="search" placeholder="Search Songs / Artists.." value={search}
+                    onChange={(e)=>{setSearch(e.target.value)}} 
+                    style={{borderRadius: '20px', 
+                        margin:'10px 0', 
+                        padding: '10px', 
+                        paddingLeft: '10px', 
+                        paddingRight: '10px', 
+                        border: 'none', 
+                        position:'absolute', 
+                        height: '35px', 
+                        maxHeight: '35px',
+                        bottom: '15%', 
+                        width: '70%',  
+                        left: '50%', 
+                        top:'70%', 
+                        transform: 'translate(-50%, -50%)'
+                    }}/>
+                </div>
+            </div>
+
+            <div style={{display:'grid',gridTemplateColumns: 'repeat(auto-fit, minmax(200px, 1fr))',gridGap: '30px', margin: '30px'}}>{ searchResults.map(track=>{
                 return <TrackSearchResult  track = {track} key = {track.uri} chooseTrack={chooseTrack}/>
             })}</div>
+
+
+        {/* <div style={{margin: '0', height: '80px', position:'relative', background: '#191414', maxWidth: '100%', }}>
+            <footer style={{margin: '0', position: 'absolute', color: 'grey', padding: '20px', left: '50%', top: '40%', transform: 'translate(-50%, -50%)'}}>Copyright &copy; </footer>
+        </div> */}
 
         </Fragment>
     );
